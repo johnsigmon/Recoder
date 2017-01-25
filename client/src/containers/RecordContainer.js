e25a14f0891716bc291a17687610da5072cb2e76
John Sigmon <johnasigmon@gmail.com>
2017-01-25 16:31:42 -0500

    ready to begin webrtc

--- client/src/containers/RecordContainer.js
+++ client/src/containers/RecordContainer.js
@@ -3,6 +3,10 @@ import { Col, Button } from 'react-bootstrap';
 /*import Poti from '../components/Poti';*/
 
 export default class RecordContainer extends Component {
+  constructor(props) {
+    super(props);
+
+  }
   render() {
     return (
       <Col md={12} style={{margin: '0', padding: '0'}}>
